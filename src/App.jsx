be88ec330bb6ee8c053c1de8d3b7a06ac36c73f3
zwenada <zwenada@my.jcu.edu.au>
2025-03-04 19:02:36 +0800

    change vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -10,42 +10,34 @@ import AdminDash from "./admin/AdminDash";
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Navbar />
-                <div id="home">
-                  <Header />
-                </div>
-                <div id="services">
-                  <Services />
-                </div>
-                <div id="about">
-                  <AboutUs />
-                </div>
-                <div id="contact">
-                  <Contact />
-                </div>
-              </>
-            }
-          />
-
-          <Route
-            path="/admin"
-            element={
-              <>
-                <AdminDash />
-              </>
-            }
-          ></Route>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <div id="home">
+                <Header />
+              </div>
+              <div id="services">
+                <Services />
+              </div>
+              <div id="about">
+                <AboutUs />
+              </div>
+              <div id="contact">
+                <Contact />
+              </div>
+              <Footer />
+            </>
+          }
+        />
+        
+        {/* Admin Dashboard Route */}
+        <Route path="/admin" element={<AdminDash />} />
+      </Routes>
+    </Router>
   );
 };
 
